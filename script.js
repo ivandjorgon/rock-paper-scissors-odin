32190a6a767dd34a2441433885ccd2ab08b11bf7
Ivan Djorgon <ivan_djorgon@hotmail.com>
2019-12-01 14:53:08 -0600

    Added capitalize helper function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Function computerPlay randomly returns either ‘Rock’, ‘Paper’ or ‘Scissors’.
 function computerPlay() {
     var myArray = [
         'Rock',
@@ -6,6 +7,12 @@ function computerPlay() {
       ];
       
     return randomItem = myArray[Math.floor(Math.random()*myArray.length)];
+}
+
+// Helper function that takes a string and returns it with only the first letter capitalized.
+// It is case insensitive and users can input rock, ROCK, RocK or any other variation.
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
 
 console.log(computerPlay());
