14d6f0e48c2fca6a52f5e3160feeb75b58d588c9
Ivan Djorgon <ivan_djorgon@hotmail.com>
2019-12-01 22:15:56 -0600

    game function initial commit

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function computerPlay() {
 
 // Function that plays a single round of Rock Paper Scissors.
 function playRound(playerSelection, computerSelection) {
-    var player = capitalize(playerSelection);
-    var computer = capitalize(computerSelection);
+    var player = toCapital(playerSelection);
+    var computer = toCapital(computerSelection);
     
     if (player == "Rock" && computer == "Rock") {
         return "Draw! Rock equals to Rock";
@@ -35,13 +35,27 @@ function playRound(playerSelection, computerSelection) {
       }
 }
 
+// Play a 5 round game that keeps the score and reports a winner or loser at the end.
+function game() {
+    var userInput = prompt("Rock Paper Scissors! Please enter your selection:", "Rock");
+    if (userInput == null || userInput == "") {
+      console.log("Player cancelled the prompt");
+    } else {
+      console.log("You played: " + userInput);
+    }
+    //var counter = {player: 0, computer: 0};
+    // Use the playRound function inside of this one
+    //playRound(playerSelection, computerSelection)
+}
+
 // Helper function that takes a string and returns it with only the first letter capitalized.
 // It is case insensitive and users can input rock, ROCK, RocK or any other variation.
-function capitalize(str) {
+function toCapital(str) {
     return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
 
 // console.log(computerPlay());
-const playerSelection = 'rock'
-const computerSelection = computerPlay()
-console.log(playRound(playerSelection, computerSelection))
+//const playerSelection = 'rock';
+//const computerSelection = computerPlay();
+//console.log(playRound(playerSelection, computerSelection));
+const playerSelection = game();
